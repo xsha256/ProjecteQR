186a7b6055ca4949b9afeb6f02d4195c66a33deb
xsha256 <43494720+xsha256@users.noreply.github.com>
2025-01-27 23:43:59 +0100

    modificar config.js

--- QRraDoc/docusaurus.config.js
+++ QRraDoc/docusaurus.config.js
@@ -15,15 +15,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ProjecteQR/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'QRra', // Usually your GitHub org/user name.
+  projectName: 'QRra', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
